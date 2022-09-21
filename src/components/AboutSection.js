90172876b2343821c6d2cdead30705db84b9c58f
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2022-09-22 01:54:19 +0300

    add image + html

---
+++ src/components/AboutSection.js
@@ -0,0 +1,15 @@
+export function AboutSection() {
+  return (
+    <section>
+      <p>What you are looking for</p>
+      <h2>We provide bespoke solutions</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate,
+        ipsum dignissimos quae laudantium asperiores nam aliquid impedit harum
+        illum dolore explicabo ab dolores itaque rerum temporibus doloribus iste
+        maiores deleniti?
+      </p>
+      <button type="button">Read More</button>
+    </section>
+  );
+}
